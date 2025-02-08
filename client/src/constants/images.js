c4ba99809826ef091bf6cfaa5e332ac776a40571
Ashikur Rafi <ashikurrafi@gmail.com>
2025-02-09 00:42:19 +0600

    cards and article done

--- client/src/constants/images.js
+++ client/src/constants/images.js
@@ -1,9 +1,13 @@
 import HeroImage from "../assets/HeroImage.svg";
 import Logo from "../assets/Logo.svg";
+import postProfileImage from "../assets/post/post-profile.svg";
+import post1Image from "../assets/post/post1.jpg";
 
 const images = {
   Logo,
   HeroImage,
+  post1Image,
+  postProfileImage,
 };
 
 export default images;
