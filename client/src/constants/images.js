689a954c1f1e17f4345293adf2edd4c7c5a82022
Ashikur Rafi <ashikurrafi@gmail.com>
2025-02-09 02:02:29 +0600

    CTA done

--- client/src/constants/images.js
+++ client/src/constants/images.js
@@ -1,3 +1,4 @@
+import CtaImage from "../assets/CtaImage.jpg";
 import HeroImage from "../assets/HeroImage.svg";
 import Logo from "../assets/Logo.svg";
 import postProfileImage from "../assets/post/post-profile.svg";
@@ -8,6 +9,7 @@ const images = {
   HeroImage,
   post1Image,
   postProfileImage,
+  CtaImage,
 };
 
 export default images;
