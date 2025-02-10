74c8e69370f9be5b843abfe1c841824478b1f7cb
Ashikur Rafi <ashikurrafi@gmail.com>
2025-02-11 01:54:18 +0600

    Logout done

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 import { MdKeyboardArrowDown } from "react-icons/md";
+import { useDispatch, useSelector } from "react-redux";
 import { images } from "../constants";
+import { logout } from "../store/actions/user";
 
-const NavItemsInfo = [
+const navItemsInfo = [
   { name: "Home", type: "link" },
   { name: "Article", type: "link" },
   { name: "Pages", type: "dropdown", items: ["About Us", "Contact Us"] },
@@ -63,6 +65,9 @@ const NavItem = ({ item }) => {
 };
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const userState = useSelector((state) => state.user);
+  const [profileDrowpdown, setProfileDrowpdown] = useState(false);
   const [navIsVisible, setNavIsVisible] = useState(false);
 
   const navVisibilityHandler = () => {
@@ -71,14 +76,17 @@ const Header = () => {
     });
   };
 
+  const logoutHandler = () => {
+    dispatch(logout());
+  };
+
   return (
     <>
       <section className="sticky top-0 left-0 right-0 z-50 bg-white">
         <header className="container mx-auto px-5 flex justify-between py-4 items-center">
           <div>
-            <img src={images.Logo} alt="logo" className="w-16" />
+            <img className="w-16" src={images.Logo} alt="logo" />
           </div>
-
           <div className="lg:hidden z-50">
             {navIsVisible ? (
               <AiOutlineClose
@@ -92,20 +100,56 @@ const Header = () => {
               />
             )}
           </div>
-
           <div
             className={`${
               navIsVisible ? "right-0" : "-right-full"
-            } transition-all duration-300 mt-[56px] lg:mt-0 bg-dark-hard lg:bg-transparent z-[49px] flex flex-col w-full lg:w-auto justify-center lg:justify-end lg:flex-row fixed top-0 bottom-0 lg:static gap-x-9 items-center`}
+            } transition-all duration-300 mt-[56px] lg:mt-0 bg-dark-hard lg:bg-transparent z-[49] flex flex-col w-full lg:w-auto justify-center lg:justify-end lg:flex-row fixed top-0 bottom-0 lg:static gap-x-9 items-center`}
           >
-            <ul className=" text-white items-center gap-y-4 lg:text-dark-soft flex flex-col lg:flex-row gap-x-2 font-semibold">
-              {NavItemsInfo.map((item) => (
+            <ul className="text-white items-center gap-y-5 lg:text-dark-soft flex flex-col lg:flex-row gap-x-2 font-semibold">
+              {navItemsInfo.map((item) => (
                 <NavItem key={item.name} item={item} />
               ))}
             </ul>
-            <button className=" mt-5 lg:mt-0 border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300">
-              Sign in
-            </button>
+            {userState.userInfo ? (
+              <div className="text-white items-center gap-y-5 lg:text-dark-soft flex flex-col lg:flex-row gap-x-2 font-semibold">
+                <div className="relative group">
+                  <div className="flex flex-col items-center">
+                    <button
+                      className="flex gap-x-1 items-center mt-5 lg:mt-0 border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300"
+                      onClick={() => setProfileDrowpdown(!profileDrowpdown)}
+                    >
+                      <span>Profile</span>
+                      <MdKeyboardArrowDown />
+                    </button>
+                    <div
+                      className={`${
+                        profileDrowpdown ? "block" : "hidden"
+                      } lg:hidden transition-all duration-500 pt-4 lg:absolute lg:bottom-0 lg:right-0 lg:transform lg:translate-y-full lg:group-hover:block w-max`}
+                    >
+                      <ul className="bg-dark-soft lg:bg-transparent text-center flex flex-col shadow-lg rounded-lg overflow-hidden">
+                        <button
+                          type="button"
+                          className="hover:bg-dark-hard hover:text-white px-4 py-2 text-white lg:text-dark-soft"
+                        >
+                          Dashboard
+                        </button>
+                        <button
+                          onClick={logoutHandler}
+                          type="button"
+                          className="hover:bg-dark-hard hover:text-white px-4 py-2 text-white lg:text-dark-soft"
+                        >
+                          Logout
+                        </button>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <button className="mt-5 lg:mt-0 border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300">
+                Sign in
+              </button>
+            )}
           </div>
         </header>
       </section>
---
+++ client/src/store/actions/user.js
@@ -0,0 +1,5 @@
+import { userActions } from "../reducers/userReducers";
+export const logout = () => (dispatch) => {
+  dispatch(userActions.resetUserInfo());
+  localStorage.removeItem("account");
+};
