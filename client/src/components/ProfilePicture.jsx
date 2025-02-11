7b6a4c6b361906d5e1c606a7020f1b94b89db56a
Ashikur Rafi <ashikurrafi@gmail.com>
2025-02-11 18:04:14 +0600

    Delete profile picture done

--- client/src/components/ProfilePicture.jsx
+++ client/src/components/ProfilePicture.jsx
@@ -1,18 +1,59 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
 import { createPortal } from "react-dom";
+import { toast } from "react-hot-toast";
 import { HiOutlineCamera } from "react-icons/hi";
-
+import { useDispatch, useSelector } from "react-redux";
 import { stables } from "../constants";
+import { updateProfilePicture } from "../services/index/users";
+import { userActions } from "../store/reducers/userReducers";
 import CropEasy from "./crop/CropEasy";
 
 const ProfilePicture = ({ avatar }) => {
+  const queryClient = useQueryClient();
+  const dispatch = useDispatch();
+  const userState = useSelector((state) => state.user);
   const [openCrop, setOpenCrop] = useState(false);
   const [photo, setPhoto] = useState(null);
+
+  const { mutate, isLoading } = useMutation({
+    mutationFn: ({ token, formData }) => {
+      return updateProfilePicture({
+        token: token,
+        formData: formData,
+      });
+    },
+    onSuccess: (data) => {
+      dispatch(userActions.setUserInfo(data));
+      setOpenCrop(false);
+      localStorage.setItem("account", JSON.stringify(data));
+      queryClient.invalidateQueries(["profile"]);
+      toast.success("Profile Photo is removed");
+    },
+    onError: (error) => {
+      toast.error(error.message);
+      console.log(error);
+    },
+  });
 
   const handleFileChange = (e) => {
     const file = e.target.files[0];
     setPhoto({ url: URL.createObjectURL(file), file });
     setOpenCrop(true);
+  };
+
+  const handleDeleteImage = () => {
+    if (window.confirm("Do you want to delete your profile picture")) {
+      try {
+        const formData = new FormData();
+        formData.append("profilePicture", undefined);
+
+        mutate({ token: userState.userInfo.token, formData: formData });
+      } catch (error) {
+        toast.error(error.message);
+        console.log(error);
+      }
+    }
   };
 
   return (
@@ -49,6 +90,7 @@ const ProfilePicture = ({ avatar }) => {
           />
         </div>
         <button
+          onClick={handleDeleteImage}
           type="button"
           className="border border-red-500 rounded-lg px-4 py-2 text-red-500"
         >
