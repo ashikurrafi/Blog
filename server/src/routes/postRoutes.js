5b360da08d3055b9c1127f1448dd4322d4350d32
Ashikur Rafi <ashikurrafi@gmail.com>
2025-02-13 01:10:34 +0600

    get post api done

--- server/src/routes/postRoutes.js
+++ server/src/routes/postRoutes.js
@@ -5,10 +5,12 @@ const {
   createPost,
   updatePost,
   deletePost,
+  getPost,
 } = require("../controllers/postControllers");
 
 router.post("/", authGuard, adminGuard, createPost);
 router.put("/:slug", authGuard, adminGuard, updatePost);
 router.delete("/:slug", authGuard, adminGuard, deletePost);
+router.get("/:slug", getPost);
 
 module.exports = router;
