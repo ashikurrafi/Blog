54e502cc17826053031f9ab7de9ba6256698917c
Ashikur Rafi <ashikurrafi@gmail.com>
2025-02-13 19:28:53 +0600

    Article Detail Page done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,18 @@
 import "./App.css";
 
+import { Route, Routes } from "react-router-dom";
+
+import ArticleDetailPage from "./pages/articleDetail/ArticleDetailPage";
 import HomePage from "./pages/home/HomePage";
 
 function App() {
   return (
     <>
       <div className="App font-display">
-        <HomePage />
+        <Routes>
+          <Route index path="/" element={<HomePage />} />
+          <Route path="/blog/:id" element={<ArticleDetailPage />} />
+        </Routes>{" "}
       </div>
     </>
   );
