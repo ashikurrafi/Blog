3666939c794c6732b2bcf670549150a0799293ea
Ashikur Rafi <ashikurrafi@gmail.com>
2025-02-13 23:30:20 +0600

    user registration done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 
+import { Toaster } from "react-hot-toast";
 import { Route, Routes } from "react-router-dom";
 
 import ArticleDetailPage from "./pages/articleDetail/ArticleDetailPage";
 import HomePage from "./pages/home/HomePage";
+import RegisterPage from "./pages/register/RegisterPage";
 
 function App() {
   return (
@@ -11,8 +13,10 @@ function App() {
       <div className="App font-display">
         <Routes>
           <Route index path="/" element={<HomePage />} />
-          <Route path="/blog/:id" element={<ArticleDetailPage />} />
+          <Route path="/blog/:slug" element={<ArticleDetailPage />} />
+          <Route path="/register" element={<RegisterPage />} />
         </Routes>
+        <Toaster />
       </div>
     </>
   );
