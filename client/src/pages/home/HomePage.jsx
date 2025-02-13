4a64de0e764620bdec86a6c95af6a5f09bdc19bf
Ashikur Rafi <ashikurrafi@gmail.com>
2025-02-13 18:58:55 +0600

    Header component Done

---
+++ client/src/pages/home/HomePage.jsx
@@ -0,0 +1,11 @@
+import MainLayout from "../../components/MainLayout";
+
+const HomePage = () => {
+  return (
+    <>
+      <MainLayout></MainLayout>
+    </>
+  );
+};
+
+export default HomePage;
