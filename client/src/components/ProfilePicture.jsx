fdcecd2c6b62aff13ef947ddfaff2359de899b0d
Ashikur Rafi <ashikurrafi@gmail.com>
2025-02-14 01:45:56 +0600

    add image upload and cropping functionality for profile picture

--- client/src/components/ProfilePicture.jsx
+++ client/src/components/ProfilePicture.jsx
@@ -1,12 +1,27 @@
+import { useState } from "react";
+import { createPortal } from "react-dom";
 import { HiOutlineCamera } from "react-icons/hi";
 
 import { stables } from "../constants";
+import CropEasy from "./crop/CropEasy";
 
 const ProfilePicture = ({ avatar }) => {
+  const [openCrop, setOpenCrop] = useState(false);
+  const [photo, setPhoto] = useState(null);
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    setPhoto({ url: URL.createObjectURL(file), file });
+    setOpenCrop(true);
+  };
   return (
     <>
+      {openCrop &&
+        createPortal(
+          <CropEasy photo={photo} setOpenCrop={setOpenCrop} />,
+          document.getElementById("portal")
+        )}
       <div className="w-full flex items-center gap-x-4">
-        <div className="relative w-20 h-20 rounded-full  outline-offset-2 outline-1 outline-primary overflow-hidden">
+        <div className="relative w-20 h-20 rounded-full outline-offset-2 outline-1 outline-primary overflow-hidden">
           <label
             htmlFor="profilePicture"
             className="cursor-pointer absolute inset-0 rounded-full bg-transparent"
@@ -23,7 +38,12 @@ const ProfilePicture = ({ avatar }) => {
               </div>
             )}
           </label>
-          <input type="file" className="sr-only" id="profilePicture" />
+          <input
+            type="file"
+            className="sr-only"
+            id="profilePicture"
+            onChange={handleFileChange}
+          />
         </div>
         <button
           type="button"
