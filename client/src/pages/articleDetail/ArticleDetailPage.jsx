8c6664f567c44720cd113e02c4e43b3ef4f5a0c6
Ashikur Rafi <ashikurrafi@gmail.com>
2025-02-17 23:13:09 +0600

    refactor ArticleDetailPage and SuggestedPosts components to fetch posts data and improve tag handling (Article sidebar) done

--- client/src/pages/articleDetail/ArticleDetailPage.jsx
+++ client/src/pages/articleDetail/ArticleDetailPage.jsx
@@ -18,44 +18,7 @@ import SocialShareButtons from "../../components/SocialShareButtons";
 import { images, stables } from "../../constants";
 import ArticleDetailSkeleton from "../../pages/articleDetail/components/ArticleDetailSkeleton";
 import SuggestedPosts from "../../pages/articleDetail/container/SuggestedPosts";
-import { getSinglePost } from "../../services/index/posts";
-
-const postsData = [
-  {
-    _id: "1",
-    image: images.Post1Image,
-    title: "Help children get better education",
-    createdAt: "2023-01-28T15:35:53.607+0000",
-  },
-  {
-    _id: "2",
-    image: images.Post1Image,
-    title: "Help children get better education",
-    createdAt: "2023-01-28T15:35:53.607+0000",
-  },
-  {
-    _id: "3",
-    image: images.Post1Image,
-    title: "Help children get better education",
-    createdAt: "2023-01-28T15:35:53.607+0000",
-  },
-  {
-    _id: "4",
-    image: images.Post1Image,
-    title: "Help children get better education",
-    createdAt: "2023-01-28T15:35:53.607+0000",
-  },
-];
-
-const tagsData = [
-  "Medical",
-  "Lifestyle",
-  "Learn",
-  "Healthy",
-  "Food",
-  "Diet",
-  "Education",
-];
+import { getAllPosts, getSinglePost } from "../../services/index/posts";
 
 const ArticleDetailPage = () => {
   const { slug } = useParams();
@@ -79,6 +42,11 @@ const ArticleDetailPage = () => {
         )
       );
     },
+  });
+
+  const { data: postsData } = useQuery({
+    queryFn: () => getAllPosts(),
+    queryKey: ["posts"],
   });
 
   return (
@@ -127,7 +95,7 @@ const ArticleDetailPage = () => {
               <SuggestedPosts
                 header="Latest Article"
                 posts={postsData}
-                tags={tagsData}
+                tags={data?.tags}
                 className="mt-8 lg:mt-0 lg:max-w-xs"
               />
               <div className="mt-7">
@@ -135,12 +103,8 @@ const ArticleDetailPage = () => {
                   Share on:
                 </h2>
                 <SocialShareButtons
-                  url={encodeURI(
-                    "https://moonfo.com/post/client-side-and-server-side-explanation"
-                  )}
-                  title={encodeURIComponent(
-                    "Client-side and Server-side explanation"
-                  )}
+                  url={encodeURI(window.location.href)}
+                  title={encodeURIComponent(data?.title)}
                 />
               </div>
             </div>
