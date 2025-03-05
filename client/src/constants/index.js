4ad84b19715208e60f36a4b7747e1a8ae6314672
Ashikur Rafi <ashikurrafi@gmail.com>
2025-03-05 11:30:44 +0600

    remove unused assets, components, and configuration files to clean up the project (restart)

--- client/src/constants/index.js
+++
@@ -1,2 +0,0 @@
-export { default as images } from "./images";
-export { default as stables } from "./stables";
--- client/src/store/actions/user.js
+++
@@ -1,5 +0,0 @@
-import { userActions } from "../reducers/userReducers";
-export const logout = () => (dispatch) => {
-  dispatch(userActions.resetUserInfo());
-  localStorage.removeItem("account");
-};
