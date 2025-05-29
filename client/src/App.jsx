3a6cfa91ed7d32596e1edbc240c368c2937b2bc3
Ashikur Rafi <ashikurrafi@gmail.com>
2025-05-29 17:52:58 +0600

    feat: update project structure with routing and add Blog and Home pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,34 @@
-import './App.css';
-import { Button } from "./components/ui/button";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
 
+import Blog from "./pages/Blog";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <Home />
+      </>
+    ),
+  },
+  {
+    path: "/blog/:id",
+    element: (
+      <>
+        <Blog />
+      </>
+    ),
+  },
+]);
 
 function App() {
-
   return (
     <>
-        <Button>Test</Button>
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/pages/Blog.jsx
@@ -0,0 +1,9 @@
+const Blog = () => {
+  return (
+    <>
+      <h1>Single blog</h1>
+    </>
+  );
+};
+
+export default Blog;
