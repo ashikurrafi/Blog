e882d95dc341aa7220b61f6e47a12d65634b0846
Ashikur Rafi <ashikurrafi@gmail.com>
2025-06-06 21:40:21 +0600

    feat: implement registration form with state management and API integration

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,4 +1,8 @@
-import { Link } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "sonner";
 import { Button } from "../components/ui/button";
 import {
   Card,
@@ -9,8 +13,41 @@ import {
 } from "../components/ui/card";
 import { Input } from "../components/ui/input";
 import { Label } from "../components/ui/label";
+import { setUser } from "../redux/authSlice";
 
 const Register = () => {
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(`/api/v1/demo/auth/registerUser`, data);
+      const result = response.data;
+      navigate("/");
+      console.log("Submitting", result);
+      toast.success("Register successful");
+      dispatch(setUser(result.data));
+    } catch (error) {
+      toast.error("Can't login");
+      console.error("Login error:", error);
+    }
+  };
+
   return (
     <>
       <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
@@ -24,14 +61,17 @@ const Register = () => {
                 </CardDescription>
               </CardHeader>
               <CardContent>
-                <form>
+                <form onSubmit={handleSubmit}>
                   <div className="flex flex-col gap-6">
                     <div className="grid gap-2">
                       <Label htmlFor="name">Full Name</Label>
                       <Input
                         id="name"
+                        name="name"
                         type="text"
                         placeholder="Name"
+                        value={data.name}
+                        onChange={handleChange}
                         required
                       />
                     </div>
@@ -39,8 +79,11 @@ const Register = () => {
                       <Label htmlFor="email">Email</Label>
                       <Input
                         id="email"
+                        name="email"
                         type="email"
                         placeholder="email@email.com"
+                        value={data.email}
+                        onChange={handleChange}
                         required
                       />
                     </div>
@@ -48,8 +91,11 @@ const Register = () => {
                       <Label htmlFor="phone">Phone Number</Label>
                       <Input
                         id="phone"
+                        name="phone"
                         type="tel"
                         placeholder="Phone Number [+880XXXXXXXXXX]"
+                        value={data.phone}
+                        onChange={handleChange}
                         required
                       />
                     </div>
@@ -57,7 +103,15 @@ const Register = () => {
                       <div className="flex items-center">
                         <Label htmlFor="password">Password</Label>
                       </div>
-                      <Input id="password" type="password" required />
+                      <Input
+                        id="password"
+                        name="password"
+                        type="password"
+                        placeholder="Password"
+                        value={data.password}
+                        onChange={handleChange}
+                        required
+                      />
                     </div>
                     <Button type="submit" className="w-full">
                       Register
@@ -65,7 +119,10 @@ const Register = () => {
                   </div>
                   <div className="mt-4 text-center text-sm">
                     Already have an account? &nbsp;
-                    <Link to={'/login'} className="underline underline-offset-4">
+                    <Link
+                      to={"/login"}
+                      className="underline underline-offset-4"
+                    >
                       Login
                     </Link>
                   </div>
